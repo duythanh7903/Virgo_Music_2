917a502c9de743dcafd528b0332eaa018f3b22ae
admin <lothanhduy2003@gmail.com>
2024-03-04 10:38:31 +0700

    hide system navigation bar device

--- app/src/main/java/com/iven/musicplayergo/BaseActivity.kt
+++ app/src/main/java/com/iven/musicplayergo/BaseActivity.kt
@@ -3,8 +3,13 @@ package com.iven.musicplayergo
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
+import android.os.Build
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.ConfigurationCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.iven.musicplayergo.preferences.ContextUtils
 import com.iven.musicplayergo.utils.Theming
 import java.util.*
@@ -34,6 +39,23 @@ abstract class BaseActivity: AppCompatActivity() {
                 return
             }
             super.attachBaseContext(ContextUtils.updateLocale(ctx, Locale.getDefault()))
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        hideSystemNavigationBar()
+        super.onWindowFocusChanged(hasFocus)
+    }
+
+    private fun hideSystemNavigationBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = WindowCompat.getInsetsController(window, window.decorView)
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
 }
